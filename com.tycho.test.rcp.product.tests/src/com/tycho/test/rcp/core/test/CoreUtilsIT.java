a8539b91415b8104e2e85c1aa20d633874c3b2dd
Gionata Boccalini <gionata.boccalini@marchesini.it>
2022-02-07 23:16:31 +0100

    Added a test plugin only for IT tests.

---
+++ com.tycho.test.rcp.product.tests/src/com/tycho/test/rcp/core/test/CoreUtilsIT.java
@@ -0,0 +1,15 @@
+package com.tycho.test.rcp.core.test;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.Test;
+
+public class CoreUtilsIT {
+
+	@Test
+	public void testCoreUtilsIT() throws NoSuchAlgorithmException, IOException {
+		System.err.println("Product IT test run!");
+	}
+	
+}
